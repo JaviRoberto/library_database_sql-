bcfa8befaf1d8181b7ca286ef39f72785efd1e51
Javier <javier.nvc@gmail.com>
2022-10-23 20:50:26 -0700

    add data for table books

---
+++ insert_data_book.sql
@@ -0,0 +1,96 @@
+INSERT INTO BOOK (BookTitle,BookAuthor,Genre) 
+VALUES ('Build your database system','1','Science' ) 
+; 
+INSERT INTO BOOK (BookTitle,BookAuthor,Genre) 
+VALUES ('The red wall','2','Fiction' ) 
+; 
+INSERT INTO BOOK (BookTitle,BookAuthor,Genre) 
+VALUES ('The perfect match','3','Fiction' ) 
+; 
+INSERT INTO BOOK (BookTitle,BookAuthor,Genre) 
+VALUES ('Digital Logic','4','Science' ) 
+; 
+INSERT INTO BOOK (BookTitle,BookAuthor,Genre) 
+VALUES ('How to be a great lawyer','5','Law' ) 
+; 
+INSERT INTO BOOK (BookTitle,BookAuthor,Genre) 
+VALUES ('Manage successful negotiations','6','Society' ) 
+; 
+INSERT INTO BOOK (BookTitle,BookAuthor,Genre) 
+VALUES ('Pollution today','7','Science' ) 
+; 
+INSERT INTO BOOK (BookTitle,BookAuthor,Genre) 
+VALUES ('A gray park','2','Fiction' ) 
+; 
+INSERT INTO BOOK (BookTitle,BookAuthor,Genre) 
+VALUES ('How to be rich in one year','8','Humor' ) 
+; 
+INSERT INTO BOOK (BookTitle,BookAuthor,Genre) 
+VALUES ('Their bright fate','9','Fiction' ) 
+; 
+INSERT INTO BOOK (BookTitle,BookAuthor,Genre) 
+VALUES ('Black lines','10','Fiction' ) 
+; 
+INSERT INTO BOOK (BookTitle,BookAuthor,Genre) 
+VALUES ('History of theater','11','Literature' ) 
+; 
+INSERT INTO BOOK (BookTitle,BookAuthor,Genre) 
+VALUES ('Electrical transformers','12','Science' ) 
+; 
+INSERT INTO BOOK (BookTitle,BookAuthor,Genre) 
+VALUES ('Build your big data system','1','Science' ) 
+; 
+INSERT INTO BOOK (BookTitle,BookAuthor,Genre) 
+VALUES ('Right and left','13','Children' ) 
+; 
+INSERT INTO BOOK (BookTitle,BookAuthor,Genre) 
+VALUES ('Programming using Python','1','Science' ) 
+; 
+INSERT INTO BOOK (BookTitle,BookAuthor,Genre) 
+VALUES ('Computer networks','14','Science' ) 
+; 
+INSERT INTO BOOK (BookTitle,BookAuthor,Genre) 
+VALUES ('Performance evaluation','15','Science' ) 
+; 
+INSERT INTO BOOK (BookTitle,BookAuthor,Genre) 
+VALUES ('Daily exercise','16','Well being' ) 
+; 
+INSERT INTO BOOK (BookTitle,BookAuthor,Genre) 
+VALUES ('The silver uniform','17','Fiction' ) 
+; 
+INSERT INTO BOOK (BookTitle,BookAuthor,Genre) 
+VALUES ('Industrial revolution','18','History' ) 
+; 
+INSERT INTO BOOK (BookTitle,BookAuthor,Genre) 
+VALUES ('Green nature','19','Well being' ) 
+; 
+INSERT INTO BOOK (BookTitle,BookAuthor,Genre) 
+VALUES ('Perfect football','20','Well being' ) 
+; 
+INSERT INTO BOOK (BookTitle,BookAuthor,Genre) 
+VALUES ('The chocolate love','21','Humor' ) 
+; 
+INSERT INTO BOOK (BookTitle,BookAuthor,Genre) 
+VALUES ('Director and leader','22','Society' ) 
+; 
+INSERT INTO BOOK (BookTitle,BookAuthor,Genre) 
+VALUES ('Play football every week','20','well being' ) 
+; 
+INSERT INTO BOOK (BookTitle,BookAuthor,Genre) 
+VALUES ('Maya the bee','13','Children' ) 
+; 
+INSERT INTO BOOK (BookTitle,BookAuthor,Genre) 
+VALUES ('Perfect rugby','20','Well being' ) 
+; 
+INSERT INTO BOOK (BookTitle,BookAuthor,Genre) 
+VALUES ('The end','23','Fiction' ) 
+; 
+INSERT INTO BOOK (BookTitle,BookAuthor,Genre) 
+VALUES ('Computer security','1','Science' ) 
+; 
+INSERT INTO BOOK (BookTitle,BookAuthor,Genre) 
+VALUES ('Participate','22','Society' ) 
+; 
+INSERT INTO BOOK (BookTitle,BookAuthor,Genre) 
+VALUES ('Positive figures','3','Fiction' ) 
+; 
