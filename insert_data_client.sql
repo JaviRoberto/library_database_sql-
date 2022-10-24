0c77c4031e832c4caf2c6e217b47e6d56bfa365d
Javier <javier.nvc@gmail.com>
2022-10-23 20:52:10 -0700

    add data for table client

---
+++ insert_data_client.sql
@@ -0,0 +1,317 @@
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Kaiden','Hill','2006','Student' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Alina','Morton','2010','Student' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Fania','Brooks','1983','Food Scientist' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Courtney','Jensen','2006','Student' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Brittany','Hill','1983','Firefighter' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Max','Rogers','2005','Student' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Margaret','XMcCarthy','1981','School Psychologist' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Julie','McCarthy','1973','Professor' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Ken','McCarthy','1974','Securities Clerk' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Britany','OQuinn','1984','Violinist' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Conner','Gardner','1998','Licensed Massage Therapist' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Mya','Austin','1960','Parquet Floor Layer' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Thierry','Rogers','2004','Student' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Eloise','Rogers','1984','Computer Security Manager' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Gerard','Gerard','1979','Oil Exploration Engineer' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Randy','Day','1986','Aircraft Electrician' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Jodie','Page','1990','Manufacturing Director' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Coral','Rice','1996','Window Washer' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Ayman','Austin','2002','Student' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Jaxson','Austin','1999','Repair Worker' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Joel','Austin','1973','Police Officer' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Alina','Austin','2010','Student' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Elin','Austin','1962','Payroll Clerk' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Ophelia','Ophelia','2004','Student' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Eliot','McGuire','1967','1967' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Peter','McKinney','1968','Professor' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Annabella','Henry','1974','Nurse' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Nurse','Baker','2001','Student' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Tyler','Baker','1984','Police Officer' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Lilian','Ross','1983','Insurance Agent' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Thierry','Thierry','1975','Bus Driver' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Angelina','Rowe','1979','1979' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Marcia','Rowe','1974','Health Educator' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Martin','Rowe','1976','Ship Engineer' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Adeline','Rowe','2005','Student' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Colette','Rowe','1963','Professor' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Diane','Clark','1975','Payroll Clerk' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Caroline','Clark','1960','Dentist' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Dalton','Clayton','1982','Police Officer' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Steve','Clayton','1990','Bus Driver' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Melanie','Clayton','1987','Computer Engineer' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Alana','Wilson','2007','Student' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Carson','Byrne','1995','Food Scientist' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Conrad','Conrad','2007','Student' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Ryan','Porter','2008','Student' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Elin','Porter','1978','Computer Programmer' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Tyler','Harvey','2007','Student' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Arya','Harvey','2008','Student' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Serena','Harvey','1978','School Teacher' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Lilly','Franklin','1976','Doctor' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Mai','Franklin','1994','Dentist' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('John','Franklin','1999','Firefighter' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Judy','Franklin','1995','Firefighter' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Katy','Lloyd','1992','School Teacher' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Tamara','Allen','1963','Ship Engineer' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Maxim','Lyons','1985','Police Officer' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Allan','Lyons','1983','Computer Engineer' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Marc','Harris','1980','School Teacher' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Elin','Young','2009','Student' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Diana','Young','2008','Student' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Diane','Young','2006','Student' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Alana','Bird','2003','Student' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Anna','Becker','1979','Security Agent' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Katie','Grant','1977','Manager' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Joan','Grant','2010','Student' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Bryan','Bell','2001','Student' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Belle','Miller','1970','Professor' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Peggy','Stevens','1990','Bus Driver' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Steve','Williamson','1975','HR Clerk' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Tyler','Williamson','1999','Doctor' ) 
+;
+
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Izabelle','Williamson','1990','Systems Analyst' ) 
+;
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Annabel','Williamson','1960','Cashier' ) 
+
+;
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Mohamed','Waters','1966','nsurance Agent' ) 
+
+;
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Marion','Newman','11970','Computer Programmer' ) 
+
+;
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Ada','Williams','1986','Computer Programmer' ) 
+
+;
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Sean','Scott','1983','	Bus Driver' ) 
+
+;
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Farrah','Scott','1974','Ship Engineer' ) 
+;
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Christine','Lambert','1973','School Teacher')
+ 
+;
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Alysha','Lambert','2007','Student' ) 
+;
+INSERT INTO Client (ClientFirstName,ClientLastName,ClientDoB,Occupation) 
+VALUES ('Maia','Grant','1984','School Teacher' ) 
+
+;
