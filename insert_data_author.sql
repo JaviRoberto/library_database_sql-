3a4f3220c4fda1f161e872ec70310c61b5d19f84
Javier <javier.nvc@gmail.com>
2022-10-23 20:49:31 -0700

    add data for table author

---
+++ insert_data_author.sql
@@ -0,0 +1,71 @@
+INSERT INTO AUTHOR (AuthorFirstName,AuthorLastName, AuthorNationality) 
+VALUES ('Sofia','Smith','Canada' ) 
+; 
+INSERT INTO AUTHOR (AuthorFirstName,AuthorLastName, AuthorNationality) 
+VALUES ('Maria','Brown','Brazil' ) 
+; 
+INSERT INTO AUTHOR (AuthorFirstName,AuthorLastName, AuthorNationality) 
+VALUES ('Elena','Martin','Mexico' ) 
+
+; INSERT INTO AUTHOR (AuthorFirstName,AuthorLastName, AuthorNationality) 
+VALUES ('Zoe','Roy','France' ) 
+
+; INSERT INTO AUTHOR (AuthorFirstName,AuthorLastName, AuthorNationality) 
+VALUES ('Sebastian','Lavoie','Canada' ) 
+
+; INSERT INTO AUTHOR (AuthorFirstName,AuthorLastName, AuthorNationality) 
+VALUES ('Dylan','Garcia','Spain' ) 
+
+; INSERT INTO AUTHOR (AuthorFirstName,AuthorLastName, AuthorNationality) 
+VALUES ('Ian','Cruz','Mexico' ) 
+
+; INSERT INTO AUTHOR (AuthorFirstName,AuthorLastName, AuthorNationality) 
+VALUES ('Lucas','Smith','USA' ) 
+
+; INSERT INTO AUTHOR (AuthorFirstName,AuthorLastName, AuthorNationality) 
+VALUES ('Fabian','Wilson','USA' ) 
+
+; INSERT INTO AUTHOR (AuthorFirstName,AuthorLastName, AuthorNationality) 
+VALUES ('Liam','Taylor','Canada' ) 
+
+; INSERT INTO AUTHOR (AuthorFirstName,AuthorLastName, AuthorNationality) 
+VALUES ('William','Thomas','Great Britain' ) 
+
+; INSERT INTO AUTHOR (AuthorFirstName,AuthorLastName, AuthorNationality) 
+VALUES ('Logan','Moore','Canada' ) 
+
+; INSERT INTO AUTHOR (AuthorFirstName,AuthorLastName, AuthorNationality) 
+VALUES ('Oliver','Martin','France' ) 
+
+; INSERT INTO AUTHOR (AuthorFirstName,AuthorLastName, AuthorNationality) 
+VALUES ('Alysha','Thompson','Canada' ) 
+
+; INSERT INTO AUTHOR (AuthorFirstName,AuthorLastName, AuthorNationality) 
+VALUES ('Isabelle','Lee','Canada' ) 
+
+; INSERT INTO AUTHOR (AuthorFirstName,AuthorLastName, AuthorNationality) 
+VALUES ('Emily','Clark','USA' ) 
+
+; INSERT INTO AUTHOR (AuthorFirstName,AuthorLastName, AuthorNationality) 
+VALUES ('John','Young','China' ) 
+
+; INSERT INTO AUTHOR (AuthorFirstName,AuthorLastName, AuthorNationality) 
+VALUES ('David','Wright','Canada' ) 
+
+; INSERT INTO AUTHOR (AuthorFirstName,AuthorLastName, AuthorNationality) 
+VALUES ('Thomas','Scott','Scott' ) 
+
+; INSERT INTO AUTHOR (AuthorFirstName,AuthorLastName, AuthorNationality) 
+VALUES ('Helena','Adams','Canada' ) 
+
+; INSERT INTO AUTHOR (AuthorFirstName,AuthorLastName, AuthorNationality) 
+VALUES ('Sofia','Carter','USA' ) 
+
+; INSERT INTO AUTHOR (AuthorFirstName,AuthorLastName, AuthorNationality) 
+VALUES ('Liam','Parker','Canada' ) 
+
+; INSERT INTO AUTHOR (AuthorFirstName,AuthorLastName, AuthorNationality) 
+VALUES ('Emily','Murphy','USA' ) 
+; 
+
+
